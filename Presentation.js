127f8ac72067eb21711eaafdd97384e3bfee7947
Kota-Kondo-1562808 <131955379+Kota-Kondo-1562808@users.noreply.github.com>
2023-07-02 22:51:16 +0900

    Update Presentation.js

--- Presentation.js
+++ Presentation.js
@@ -1,98 +1,88 @@
-'use strict'
+'use strict';
+
 // 1行目に記載している 'use strict' は削除しないでください
 
-//クリップボードから画像を貼り付ける
-//参照URL https://www.techiedelight.com/ja/paste-image-from-clipboard-using-javascript/
+// クリップボードから画像を貼り付ける
+// 参照URL https://www.techiedelight.com/ja/paste-image-from-clipboard-using-javascript/
 
-window.alert("最初にグラフ画像を張り付けてください ※グラフの軸のギリギリをコピーして貼り付けて下さい。");
 document.onpaste = function(event) {
-  //最初のアイテムを検討します(複数のアイテムに簡単に拡張できます)
+  // 最初のアイテムを検討します(複数のアイテムに簡単に拡張できます)
   let item = event.clipboardData.items[0];
 
   if (item.type.indexOf("image") === 0) {
-      let blob = item.getAsFile();
+    let blob = item.getAsFile();
 
-      let reader = new FileReader();
-      reader.onload = function(event) {
-          document.getElementById("container").src = event.target.result;
-        };    
-        reader.readAsDataURL(blob);
+    let reader = new FileReader();
+    reader.onload = function(event) {
+      document.getElementById("container").src = event.target.result;
+    };
+    reader.readAsDataURL(blob);
   }
-}
-
-// const img = document.getElementById("container");
-// function imgLoad() {
-//   prompt("X軸の最大値を入力して下さい");
-//   // prompt("Y軸の最大値を入力して下さい");
-// }
-
-
-
-
-
-//一つの処理を実行するごとに次やる内容をアラートする。
-
-
-//画像データの原点、X軸終点、Y軸終点をクリックすることで絶対座標を取得する。
-//絶対座標はHTMLの左上が原点　やっかい　3回実施（原点、X軸、Y軸）
-// const origin = document.body.addEventListener("click", function(event) {
-// 	let x = event.pageX;
-// 	let y = event.pageY;
-// });
-
-// const xCood = document.body.addEventListener("click", function(event) {
-// 	let x = event.pageX;
-// 	let y = event.pageY;
-// });
-
-// const yCood = document.body.addEventListener("click", function(event) {
-// 	let x = event.pageX;
-// 	let y = event.pageY;
-// });
+};
 
 let origin;
 let xCoord;
 let yCoord;
+const container = document.getElementById("container");
 
-document.getElementById("container").addEventListener("click", function(event) {
-	let x = event.pageX;
-	let y = event.pageY;
-  
-  if (!origin) {
-    origin = {x, y};
+container.addEventListener("click", function(event) {
+  // クリックした点のHTML上の左上からの相対座標を取得
+  let clickX = event.pageX;
+  let clickY = event.pageY;
+
+  // 画像データ(container)のleft, top, right, bottom, x, y, width, heightの各プロパティを取得
+  const graphRect = container.getBoundingClientRect();
+
+  // クリックしたX座標からグラフの左端の隙間を引く
+  const xPosition = clickX - graphRect.left;
+  // (HTMLの上端からグラフの下端)からクリックしたY座標を引く
+  const yPosition = graphRect.bottom - clickY;
+
+  //原点、X軸終点、Y軸終点の座標を取得
+  if (!origin) { //原点が取得されていないときに実行される
+    origin = { x: xPosition, y: yPosition };
     alert(`原点の座標：X=${origin.x}, Y=${origin.y} を取得しました。
     ※※画像貼り付け前にクリック、原点以外をクリックした場合はWebページをリロードし最初からやり直して下さい※※`);
-  } else if (!xCoord) {
-    xCoord = {x, y};
+  } else if (!xCoord) {  //原点は取得、X軸終点が取得されていないときに実行される
+    xCoord = { x: xPosition, y: yPosition };
     alert(`X軸終点の座標：X=${xCoord.x}, Y=${xCoord.y} を取得しました。
     ※※X軸終点以外をクリックした場合はWebページをリロードし最初からやり直して下さい※※`);
-  } else if (!yCoord) {
-    yCoord = {x, y};
+  } else if (!yCoord) {  //原点、X軸終点は取得、Y軸終点が取得されていないときに実行される
+    yCoord = { x: xPosition, y: yPosition };
     alert(`Y軸終点の座標：X=${yCoord.x}, Y=${yCoord.y} を取得しました。
     ※※Y軸終点以外をクリックした場合はWebページをリロードし最初からやり直して下さい※※`);
+  } else {  //原点、X,Y軸が取得されたら座標を表示。
+    // ユーザーにX軸の最大値、Y軸の最大値を入力してもらう。その値を取得する。
+    const xAxisMax = document.getElementById("inputX");
+    const yAxisMax = document.getElementById("inputY");
+  
+    // 1px当たりのグラフ上の数値を計算
+    const xScale = xAxisMax.value / (xCoord.x - origin.x);
+    const yScale = yAxisMax.value / (yCoord.y - origin.y);
+  
+    // グラフをクリックした座標を補正した値に変換
+    const xValue = (xPosition - origin.x) * xScale;
+    const yValue = (yCoord.y - clickY) * yScale;
+  
+    // 座標を表示
+    const outPutCoordinatesX = document.getElementById('click-coordinatesX');
+    outPutCoordinatesX.innerHTML += `${xValue.toFixed(0)}\n`;
+
+    const outPutCoordinatesY = document.getElementById('click-coordinatesY');
+    outPutCoordinatesY.innerHTML += `${yValue.toFixed(0)}\n`;
+
+  //表示されたX軸の座標をコピーする。
+    const getXCoord = document.getElementById("get-xcoord");
+    getXCoord.addEventListener("click", () => {
+    //クリップボードにコピー
+      navigator.clipboard.writeText(outPutCoordinatesX.textContent);
+    });
+
+    const getYCoord = document.getElementById("get-ycoord");
+    getYCoord.addEventListener("click", () => {
+    //クリップボードにコピー
+      navigator.clipboard.writeText(outPutCoordinatesY.textContent);
+    });
   }
+  
 });
-
-const xAxisMaxValue = document.getElementById("inputX");
-const yAxisMaxValue = document.getElementById("inputY");
-
-
-
-
-//html グラフのｘ軸の最大値を入力するinput
-//html グラフのY軸の最大値を入力するinput作成
-//ユーザーが入力した最大値を取得する
-
-//ユーザーが入力した値と絶対座標の関係から1pxはどれだけの値になるか計算する関数を作成
-
-
-//上の関数を使って画像をクリックするとクリックした箇所のxy座標を取得する。
-
-
-//取得したxy座標は下の方に表示される。
-
-// ① ユーザーが画像を貼り付ける
-// ② 画像データ（グラフ波形）の原点、X軸最大値、Y軸最大値をクリックし、その位置データを取得
-// ③ ユーザーにX軸の最大値、Y軸の最大値を入力してもらう
-// ④ ②と③から取得したデータから1px当たりのグラフ上の数値を計算する。
-// ⑤ ユーザーがグラフをクリックするとクリックした座標がHTML上に出力される。（このときの座標は④で補正された値）　
